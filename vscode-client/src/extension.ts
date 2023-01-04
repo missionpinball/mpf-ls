d4099073f281554480db9e714d38525665dbd988
John Marsh <john.marsh@gmail.com>
2023-01-04 13:45:35 -0500

    Updated libs and tweaked functionality

--- vscode-client/src/extension.ts
+++ vscode-client/src/extension.ts
@@ -4,48 +4,35 @@
  * ------------------------------------------------------------------------------------------ */
 'use strict';
 
-import * as net from 'net';
+import { workspace, ExtensionContext, window } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions, } from 'vscode-languageclient/node';
 
-import { workspace, Disposable, ExtensionContext } from 'vscode';
-import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, ErrorAction, ErrorHandler, CloseAction, TransportKind } from 'vscode-languageclient';
-
-function startLangServer(command: string, args: string[], documentSelector: string[]): Disposable {
-	const serverOptions: ServerOptions = {
-        command,
-        args,
-	};
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: documentSelector,
-        synchronize: {
-            configurationSection: "mpfls"
-        }
-	}
-	return new LanguageClient(command, serverOptions, clientOptions).start();
-}
-
-function startLangServerTCP(addr: number, documentSelector: string[]): Disposable {
-	const serverOptions: ServerOptions = function() {
-		return new Promise((resolve, reject) => {
-			var client = new net.Socket();
-			client.connect(addr, "127.0.0.1", function() {
-				resolve({
-					reader: client,
-					writer: client
-				});
-			});
-		});
-	}
-
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: documentSelector,
-	}
-	return new LanguageClient(`tcp lang server (port ${addr})`, serverOptions, clientOptions).start();
-}
+let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-    const executable = workspace.getConfiguration("mpfls").get<string>("executable");
-    context.subscriptions.push(startLangServer(executable, ["-vv"], ["yaml"]));
-    // For TCP server needs to be started seperately
-    // context.subscriptions.push(startLangServerTCP(2087, ["yaml"]));
+    const command = workspace.getConfiguration("mpfls").get<string>("executable");
+	if(command) {
+		const args = ["-vv"];
+		const serverOptions: ServerOptions = {
+			command,
+			args,
+		};
+		const clientOptions: LanguageClientOptions = {
+			documentSelector: ['yaml'],
+			synchronize: {
+				configurationSection: "mpfls"
+			}
+		}
+		client = new LanguageClient("mpfls", "mpfls", serverOptions, clientOptions);
+		client.start();
+	} else {
+		window.showErrorMessage('Lang Server Executable Config was not found!');
+	}
 }
 
+export function deactivate(): Thenable<void> | undefined {
+	if(!client) {
+		return undefined;
+	}
+	return client.stop();
+}
